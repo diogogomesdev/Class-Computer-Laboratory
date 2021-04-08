a71a695575fb6cc533bab3d0c31f932a2df06e28
diogo_dev <78478056+diogogomesdev@users.noreply.github.com>
2021-04-08 15:00:40 +0100

    Update [1] exercises.cpp

--- [1] exercises.cpp
+++ [1] exercises.cpp
@@ -4,3 +4,28 @@ int contar_maiores(int* vec, int size, int val){
         if(vec[i] > val) count++;
     return count;
 }
+
+int palindromo(char* str){
+
+    int size = 0;
+    int i=0;
+
+    // size array of chars
+    while(str[i] != '\0'){
+        size++;
+        i++;
+    }
+
+    int half = size / 2, last = size - 1, count=0;
+
+    //check if is palindrome
+    for(int i=0; i<half; i++){
+        if(str[i] == str[last]){
+            count++;
+            last--;
+        }
+    }
+
+    if(count == half) return 1;
+    else return 0;
+}
