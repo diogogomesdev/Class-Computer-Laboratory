a4ff837d9fcc5fcad6092ec2a27b84abd173b092
diogo_dev <78478056+diogogomesdev@users.noreply.github.com>
2021-04-09 08:02:28 +0100

    Update [1] work.cpp

--- [1] work.cpp
+++ [1] work.cpp
@@ -85,6 +85,198 @@ void vertical_rotation_to_file(struct ppm_image img, char** argv){
     fclose(fo);
 }
 
+void component_adjust(struct ppm_image img, char** argv){
+
+    int size1 = size(argv[1]);
+    int size2 = size(argv[2]);
+    int size3 = size(argv[3]);
+
+    int somar1 = 0, somar2=0, somar3=0;
+
+    int x=1;
+    if(argv[1][0] == '-'){
+        for(int i=size1-1;i>=1;i--){
+            somar1 += ((argv[1][i]-48) * x);
+            x *= 10;
+        }
+        somar1 = -somar1;
+    }
+    else{
+        if(argv[1][0] == '+'){
+            for(int i=size1-1;i>=1;i--){
+                somar1 += ((argv[1][i]-48) * x);
+                x *= 10;
+            }
+        }
+        else{
+            for(int i=size1-1;i>=0;i--){
+                somar1 += ((argv[1][i]-48) * x);
+                x *= 10;
+            }
+        }
+    }
+
+    x=1;
+    if(argv[2][0] == '-'){
+        for(int i=size2-1;i>=1;i--){
+            somar2 += ((argv[2][i]-48) * x);
+            x *= 10;
+        }
+        somar2 = -somar2;
+    }
+    else{
+        if(argv[2][0] == '+'){
+            for(int i=size2-1;i>=1;i--){
+                somar2 += ((argv[2][i]-48) * x);
+                x *= 10;
+            }
+        }
+        else{
+            for(int i=size2-1;i>=0;i--){
+                somar2 += ((argv[2][i]-48) * x);
+                x *= 10;
+            }
+        }
+    }
+
+    x=1;
+    if(argv[3][0] == '-'){
+        for(int i=size3-1;i>=1;i--){
+            somar3 += ((argv[3][i]-48) * x);
+            x *= 10;
+        }
+        somar3 = -somar3;
+    }
+    else{
+        if(argv[3][0] == '+'){
+            for(int i=size3-1;i>=1;i--){
+                somar3 += ((argv[3][i]-48) * x);
+                x *= 10;
+            }
+        }
+        else{
+            for(int i=size3-1;i>=0;i--){
+                somar3 += ((argv[3][i]-48) * x);
+                x *= 10;
+            }
+        }
+    }
+    int new1, new2, new3;
+
+    printf("%c%c\n", img.type[0], img.type[1]);
+    printf("%d ", img.width);
+    printf("%d\n", img.heigth);
+    printf("%d\n", img.rgb);
+
+    for(int i=0;i<img.heigth;i++){
+        for(int j=0;j<img.width;j++){
+
+            new1 = img.matrix[i][j].R + somar1;
+            if(new1 > img.rgb) new1 = img.rgb;
+            if(new1 < 0) new1 = 0;
+            printf("%d ", new1);
+
+            new2 = img.matrix[i][j].G + somar2;
+            if(new2 > img.rgb) new2 = img.rgb;
+            if(new2 < 0) new2 = 0;
+            printf("%d ", new2);
+
+            new3 = img.matrix[i][j].B + somar3;
+            if(new3 > img.rgb) new3 = img.rgb;
+            if(new3 < 0) new3 = 0;
+            printf("%d\n", new3);
+        }
+    }
+}
+
+void component_adjust_to_file(struct ppm_image img, char** argv){
+    FILE* fo;
+
+    if((fo = fopen(argv[5], "w")) == NULL){
+        exit(1);
+        printf("Erro ao ler ficheiro\n");
+    }
+
+    int size1 = size(argv[1]);
+    int size2 = size(argv[2]);
+    int size3 = size(argv[3]);
+
+    int somar1 = 0, somar2=0, somar3=0;
+
+    int x=1;
+    if(argv[1][0] == '-'){
+        for(int i=size1-1;i>=1;i--){
+            somar1 += ((argv[1][i]-48) * x);
+            x *= 10;
+        }
+        somar1 = -somar1;
+    }
+    else{
+        for(int i=size1-1;i>=0;i--){
+            somar1 += ((argv[1][i]-48) * x);
+            x *= 10;
+        }
+    }
+
+    x=1;
+    if(argv[2][0] == '-'){
+        for(int i=size2-1;i>=1;i--){
+            somar2 += ((argv[2][i]-48) * x);
+            x *= 10;
+        }
+        somar2 = -somar2;
+    }
+    else{
+        for(int i=size3-1;i>=0;i--){
+            somar2 += ((argv[2][i]-48) * x);
+            x *= 10;
+        }
+    }
+
+    x=1;
+    if(argv[3][0] == '-'){
+        for(int i=size1-1;i>=1;i--){
+            somar3 += ((argv[3][i]-48) * x);
+            x *= 10;
+        }
+        somar3 = -somar3;
+    }
+    else{
+        for(int i=size1-1;i>=0;i--){
+            somar3 += ((argv[3][i]-48) * x);
+            x *= 10;
+        }
+    }
+    int new1=0, new2=0, new3=0;
+
+    fprintf(fo, "%c%c\n", img.type[0], img.type[1]);
+    fprintf(fo, "%d ", img.width);
+    fprintf(fo, "%d\n", img.heigth);
+    fprintf(fo, "%d\n", img.rgb);
+    for(int i=0;i<img.heigth;i++){
+        for(int j=0;j<img.width;j++){
+            new1 = img.matrix[i][j].R + somar1;
+            if(new1 > img.rgb) new1 = img.rgb;
+            if(new1 < 0) new1 = 0;
+            fprintf(fo, "%d ", new1);
+
+            new2 = img.matrix[i][j].G + somar2;
+            if(new2 > img.rgb) new2 = img.rgb;
+            if(new2 < 0) new2 = 0;
+            fprintf(fo, "%d ", new2);
+
+            new3 = img.matrix[i][j].B + somar3;
+            if(new3 > img.rgb) new3 = img.rgb;
+            if(new3 < 0) new3 = 0;
+            fprintf(fo, "%d\n", new3);
+
+        }
+    }
+
+    fclose(fo);
+}
+
+
 int main(int argc, char** argv){
 
     struct ppm_image image;
